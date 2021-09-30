1e644ac4defe7153ea521f010882c96567b15ab6
Nicolas Pasqual <77816504+nicolaspasqual@users.noreply.github.com>
2021-09-30 08:27:49 -0300

    Update node_exporter_docker_version.sh

--- check_version_docker_node_exporter/node_exporter_docker_version.sh
+++ check_version_docker_node_exporter/node_exporter_docker_version.sh
@@ -1,20 +1,23 @@
 #!/bin/bash
  
 #--------------------------------VARIAVEIS--------------------------------------------------
- 
+
 CONTAINER_ID="/opt/scripts/containers_ID.txt"
 CONTAINER_VERSION="/opt/scripts/versão_id.txt"
- 
+
+
 #--------------------------------EXECUÇÃO----------------------------------------------------
- 
+rm /var/lib/prometheus/node-exporter/*.prom
+
 docker ps -a --format "{{.ID}}" > /opt/scripts/containers_ID.txt
  
 docker container inspect $(cat $CONTAINER_ID) | jq -r '.[].Config.Image' | sed 's/\./_/;s/\./_/;s/\:/_/' > /opt/scripts/versão_id.txt
- 
+
 while read -r linha 
 do 
-touch /var/lib/prometheus/node-exporter/"$linha".prom
+linha2=$( echo "$linha" | cut -d _ -f1)
+touch /var/lib/prometheus/node-exporter/"$linha2".prom
 echo "# HELP to find docker version DOCKER metric docker_version
 #TYPE docker_version
-docker_version_"$linha" 0" > /var/lib/prometheus/node-exporter/"$linha".prom
+docker_version_"$linha" 0" > /var/lib/prometheus/node-exporter/"$linha2".prom
 done < /opt/scripts/versão_id.txt
